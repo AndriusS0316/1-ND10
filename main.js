507e2cdd33626e117cfe4c3ff9a00f66e7c4de21
Andrius <andriuss0316@gmail.com>
2021-08-03 12:56:55 +0300

    20210803

--- main.js
+++ main.js
@@ -1,31 +1,16 @@
-// astunta uzduotis
+// desimta uzduotis
 
 let rezultatas = 0;
 
-const minusDOM = document.querySelector(".minus");
-const plusDOM = document.querySelector(".plus");
-const numberDOM = document.querySelector(".number");
-const resetDOM = document.querySelector(".reset");
+const submit = document.querySelector("button");
+const inputt = document.querySelector("input");
+const spann = document.querySelector("span");
 
-function minusClick() {
-    rezultatas = rezultatas - 1;
-    numberDOM.innerText = rezultatas;
+function submitClick() {
+    spann.value = inputt.innerText;
 
     // galima buvo rasyti = numberDOM.innerText = --rezultatas
 }
 
-function plusCick() {
-    rezultatas = rezultatas + 1;
-    numberDOM.innerText = rezultatas;
 
-    // galima buvo rasyti = numberDOM.innerText = ++rezultatas
-}
-
-function resetClick() {
-    rezultatas = 0;
-    numberDOM.innerText = rezultatas;
-}
-
-minusDOM.addEventListener('click', minusClick);
-plusDOM.addEventListener('click', plusCick);
-resetDOM.addEventListener('click', resetClick);
+minusDOM.addEventListener('click', submitClick);
